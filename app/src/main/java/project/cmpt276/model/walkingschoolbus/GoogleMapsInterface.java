eaa127f599609c980aef002f1d4c324a7d6a1dbe
Jorawar Chana <jorawar.chana@gmail.com>
2018-03-04 13:19:39 -0800

    Implemented minor functions for maps interface - get and set radius

--- app/src/main/java/project/cmpt276/model/walkingschoolbus/GoogleMapsInterface.java
+++ app/src/main/java/project/cmpt276/model/walkingschoolbus/GoogleMapsInterface.java
@@ -6,6 +6,20 @@ package project.cmpt276.model.walkingschoolbus;
 
 public class GoogleMapsInterface {
     private int radius;
+
+    public int getRadius(){
+        return radius;
+    }
+
+    public void setRadius(int radius){
+        if(radius > 0){
+            this.radius = radius;
+        }
+        else{
+            // Just a default value, should be moved to a const later
+            this.radius = 100;
+        }
+    }
     /*
     *
     * getCurrentLocation
