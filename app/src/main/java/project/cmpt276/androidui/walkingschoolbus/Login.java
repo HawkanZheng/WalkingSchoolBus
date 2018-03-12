34a61021befa1994d022899e7f34822258401309
Balraj Virdee <bvirdee@sfu.ca>
2018-03-11 18:53:42 -0700

    added error checking to login and create user activities

--- app/src/main/java/project/cmpt276/androidui/walkingschoolbus/Login.java
+++ app/src/main/java/project/cmpt276/androidui/walkingschoolbus/Login.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 import project.cmpt276.model.walkingschoolbus.GoogleMapsInterface;
@@ -12,12 +14,19 @@ import project.cmpt276.model.walkingschoolbus.GoogleMapsInterface;
 public class Login extends AppCompatActivity {
 
     public GoogleMapsInterface gmaps;
+    String password;
+    String userName;
+
+    EditText getPassword;
+    EditText getUserName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         gmaps = GoogleMapsInterface.getInstance(this);
+        getInput();
         setUpSkipButton();
         setUpSignUpButton();
     }
@@ -30,8 +39,15 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(Login.this, menu.class);
-                startActivity(intent);
+                setUserInfo();
+
+                if(!errorCheck())
+                {
+                    //look for the user in the server and proceed accordingly
+                    Intent intent = new Intent(Login.this, menu.class);
+                    startActivity(intent);
+                }
+
 
             }
         });
@@ -52,6 +68,55 @@ public class Login extends AppCompatActivity {
 
             }
         });
+    }
+
+    private boolean errorCheck()
+    {
+        boolean hasError = false;
+        String errors = "please correct the following\n";
+
+        if(userName.length()==0)
+        {
+            errors = errors +"User Name Invalid\n";
+            hasError = true;
+        }
+
+        if(password.length()==0)
+        {
+            errors = errors+"Password Invalid\n";
+            hasError=true;
+        }
+
+
+        if(hasError)
+        {
+            changeError(errors);
+        }
+
+
+        return hasError;
+    }
+
+    private void getInput()
+    {
+        getUserName = (EditText) findViewById(R.id.userName);
+        getPassword = (EditText) findViewById(R.id.enterPassWord);
+
+    }
+
+    private void setUserInfo()
+    {
+        userName = getUserName.getText().toString();
+        password = getPassword.getText().toString();
+
+    }
+
+
+    private void changeError(String error)
+    {
+        TextView test = findViewById(R.id.loginErrorMessages);
+        test.setText(error);
+
     }
 
 
--- app/src/main/java/project/cmpt276/androidui/walkingschoolbus/signUp.java
+++ app/src/main/java/project/cmpt276/androidui/walkingschoolbus/signUp.java
@@ -5,49 +5,113 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class signUp extends AppCompatActivity {
 
-//    TextView errorMessage = (TextView) findViewById(R.id.errorMessages);
+
+//    User user = new User();
 //    TextView name = (TextView) findViewById(R.id.getName);
 //    String toName = name.toString();
+
+    String name;
+    String password;
+    String userName;
+
+    EditText getName;
+    EditText getPassword;
+    EditText getUserName;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getInput();
         setUpJoinButton();
     }
 
 
     private void setUpJoinButton()
     {
+
         Button button = (Button) findViewById(R.id.join);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //if(!errorCheck())
-                //{
+                setUserInfo();
+                if(!errorCheck())
+                {
+                    //create a new user
                     Intent intent = new Intent(signUp.this, menu.class);
                     startActivity(intent);
-                //}
+                }
             }
         });
     }
 
 
 //
-//    private boolean errorCheck()
-//    {
-//        if(toName.length()==0)
-//        {
-//            errorMessage.setText("name must be at least one character long\n");
-//            return true;
-//        }
-//
-//
-//        return false;
-//    }
+    private boolean errorCheck()
+    {
+        boolean hasError = false;
+        String errors = "please correct the following\n";
+
+        if(name.length()==0)
+        {
+            errors = errors+"Name Invalid\n";
+            hasError= true;
+        }
+
+        if(userName.length()==0)
+        {
+            errors = errors +"User Name Invalid\n";
+            hasError = true;
+        }
+
+        if(password.length()==0)
+        {
+            errors = errors+"Password Invalid\n";
+            hasError=true;
+        }
+
+
+        if(hasError)
+        {
+            changeError(errors);
+        }
+
+
+        return hasError;
+    }
+
+    private void getInput()
+    {
+        getUserName = (EditText) findViewById(R.id.createEmail);
+        getPassword = (EditText) findViewById(R.id.createPassword);
+        getName = (EditText) findViewById(R.id.createName);
+    }
+
+    private void setUserInfo()
+    {
+        userName = getUserName.getText().toString();
+        password = getPassword.getText().toString();
+        name = getName.getText().toString();
+    }
+
+
+    private void changeError(String error)
+    {
+        TextView test = findViewById(R.id.errorMessages);
+        test.setText(error);
+
+    }
 }
