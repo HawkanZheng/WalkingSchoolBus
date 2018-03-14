b066a6eb2a1d41ceb989eeeaade25bc153350df6
Jerry <jhf2@sfu.ca>
2018-03-13 21:05:11 -0700

    Temporary fix to having locations disabled.
    Will crash if you start the maps activity too quickly after enabling locations.

--- app/src/main/java/project/cmpt276/androidui/walkingschoolbus/MapsActivity.java
+++ app/src/main/java/project/cmpt276/androidui/walkingschoolbus/MapsActivity.java
@@ -1,15 +1,17 @@
 package project.cmpt276.androidui.walkingschoolbus;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -18,12 +20,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -34,15 +44,15 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 import project.cmpt276.model.walkingschoolbus.GoogleMapsInterface;
@@ -60,8 +70,9 @@ import static com.google.android.gms.maps.model.BitmapDescriptorFactory.HUE_BLUE
 import static com.google.android.gms.maps.model.BitmapDescriptorFactory.HUE_GREEN;
 import static com.google.android.gms.maps.model.BitmapDescriptorFactory.HUE_RED;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,ActivityCompat.OnRequestPermissionsResultCallback{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,ActivityCompat.OnRequestPermissionsResultCallback {
 
+    private static final int REQUEST_CHECK_SETTINGS = 100 ;
     private GroupCollection groupCollection = GroupCollection.getInstance();
     private ArrayList<Group> groupInRadius = new ArrayList<Group>();
     private ArrayList<Marker> groupMarkersPlaced = new ArrayList<Marker>();
@@ -93,13 +104,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private LocationCallback locationCallback;
     private LocationSettingsRequest locationSettings;
     private Circle userRadius;
+    private GoogleApiClient gApi;
 
     //Variables in use
     private static final int LOCATION_PERMISSION_REQUESTCODE = 076;
     private final String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
     private static int markerId = 1;
     private static final long LOCATION_UPDATE_RATE_IN_MS = 10000;
-
 
     private WGServerProxy proxy;
     private GroupCollection groupList;
@@ -115,10 +126,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         locationService = LocationServices.getFusedLocationProviderClient(this);
         gMapsInterface =  GoogleMapsInterface.getInstance(this);
-        createLocationRequest();
-        createLocationCallback();
-        createLocationSettings();
-
+        settingsRequest();
         // Setup  buttons -- These need to come after the map creation
         setupSaveButton();
         setupJoinGroupButton();
@@ -135,8 +143,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //mMap.addMarker(placeMarkerAtLocation(origin, GROUP_TYPE, "Origin"));
         //Moves the camera to your location and pins it.
         //Also displays nearby groups
-        addUserBlip();
-
+        //settingsRequest();
         //Place Marker when long pressing on map.
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
@@ -209,6 +216,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
     }
+    public void settingsRequest() {
+        createLocationRequest();
+        createLocationCallback();
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+        task.addOnSuccessListener(this, e -> addUserBlip());
+        task.addOnFailureListener(this, e -> locationsNotOn((ApiException) e));
+    }
+
+    private void locationsNotOn(ApiException e){
+        switch (e.getStatusCode()) {
+            case CommonStatusCodes.RESOLUTION_REQUIRED:
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    resolvable.startResolutionForResult(this, REQUEST_CHECK_SETTINGS);
+                    finish(); //Temporary solution.
+                } catch (IntentSender.SendIntentException sendEx) {
+                    // Ignore the error.
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                // Location settings are not satisfied. However, we have no way
+                // to fix the settings so we won't show the dialog.
+                break;
+        }
+    }
 
     private void addUserBlip() {
         if (ActivityCompat.checkSelfPermission(this, perms[0]) == PackageManager.PERMISSION_GRANTED) {
@@ -265,7 +306,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * https://developer.android.com/training/location/receive-location-updates.html
      **/
 
-
     @Override
     public void onResume(){
         super.onResume();
@@ -275,7 +315,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //Initialize the locationRequest object.
     private void createLocationRequest(){
         locationRequest = new LocationRequest()
-                //High accuracy may be power consuming according to the API
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(LOCATION_UPDATE_RATE_IN_MS);
     }
@@ -305,6 +344,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         builder.addLocationRequest(locationRequest);
         locationSettings = builder.build();
     }
+
 
     //Begin listening for updates.
     private void startLocationUpdates(){
