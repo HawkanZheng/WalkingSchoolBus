d5214cc6ed9133dfbd8a2b1ab8732d972503354e
Jorawar Chana <jorawar.chana@gmail.com>
2018-03-22 22:32:25 -0700

    Added Messaging Activity - Added access to activity - Added UI - Minor refactor changes

---
+++ app/src/main/java/project/cmpt276/androidui/walkingschoolbus/MessagingActivity.java
@@ -0,0 +1,34 @@
+package project.cmpt276.androidui.walkingschoolbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MessagingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+
+        setupActionBarBack();
+    }
+
+
+    // Add a Back button on the Action Bar
+    private void setupActionBarBack() {
+        // set the button to be visible
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // On back button click, finish the activity
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
