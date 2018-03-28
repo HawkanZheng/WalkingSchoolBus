37e0fe6fafdf34f55cc4b9893096611542042fe5
Balraj Virdee <bvirdee@sfu.ca>
2018-03-27 22:02:37 -0700

    user can now edit all non-spinner fields. (changes are still only local)

--- app/src/main/java/project/cmpt276/androidui/walkingschoolbus/editUserInfo.java
+++ app/src/main/java/project/cmpt276/androidui/walkingschoolbus/editUserInfo.java
@@ -24,6 +24,10 @@ public class editUserInfo extends AppCompatActivity {
     private SharedValues sharedValues;
 
     private EditText updateAddress;
+    private EditText updateHomePhone;
+    private EditText updateCellPhone;
+    private EditText updateTeacherName;
+    private EditText updateEmergencyInfo;
 
 
     @Override
@@ -31,24 +35,9 @@ public class editUserInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user_info);
         user = User.getInstance();
-        updateAddress  = findViewById(R.id.updateAddress);
-        updateAddress.setText(user.getAddress(), TextView.BufferType.EDITABLE);
-//
 
-//        sharedValues = SharedValues.getInstance();
-//        proxy = ProxyBuilder.getProxy(getString(R.string.apiKey), sharedValues.getToken());
-
-//        Log.i(TAG, ""+user.toString());
-//        groupList = GroupCollection.getInstance();
-
-        setupGreeting();
+        setCurrentInfo();
         setUpDoneButton();
-    }
-
-
-    private void setupGreeting() {
-        TextView view = findViewById(R.id.testGreeting);
-        view.setText(user.getName() + " " + user.getTeacherName() + " " + " you live at " + user.getAddress());
     }
 
 
@@ -59,16 +48,53 @@ public class editUserInfo extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String newAddress = updateAddress.getText().toString();
 
-                if(!newAddress.equals(""))
-                {
-                    user.setAddress(newAddress);
 
-                }
+
+                setNewInfo();
 
                 finish();
             }
         });
+    }
+
+    private void setCurrentInfo()
+    {
+        updateAddress = findViewById(R.id.updateAddress);
+        updateAddress.setText(user.getAddress(), TextView.BufferType.EDITABLE);
+
+        updateHomePhone = findViewById(R.id.updateHomeNumber);
+        updateHomePhone.setText(user.getHomePhone(), TextView.BufferType.EDITABLE);
+
+        updateCellPhone = findViewById(R.id.updateCellNumber);
+        updateCellPhone.setText(user.getCellPhone(), TextView.BufferType.EDITABLE);
+
+        updateTeacherName = findViewById(R.id.updateTeacher);
+        updateTeacherName.setText(user.getTeacherName(), TextView.BufferType.EDITABLE);
+
+        updateEmergencyInfo = findViewById(R.id.updateEmergencyInfo);
+        updateEmergencyInfo.setText(user.getEmergencyContactInfo(), TextView.BufferType.EDITABLE);
+
+    }
+
+
+    private void setNewInfo()
+    {
+        String newAddress = updateAddress.getText().toString();
+        user.setAddress(newAddress);
+
+        String newHomePhone = updateHomePhone.getText().toString();
+        user.setHomePhone(newHomePhone);
+
+        String newCellPhone = updateCellPhone.getText().toString();
+        user.setCellPhone(newCellPhone);
+
+        String newTeacherName = updateTeacherName.getText().toString();
+        user.setTeacherName(newTeacherName);
+
+        String newEmergencyInfo = updateEmergencyInfo.getText().toString();
+        user.setEmergencyContactInfo(newEmergencyInfo);
+
+
     }
 }
