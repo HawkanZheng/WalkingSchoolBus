cea1eb67d81d574ec5701ca820275465f1f4239a
Balraj Virdee <bvirdee@sfu.ca>
2018-03-27 20:29:37 -0700

    user can edit info, but changes are only saved locally

--- app/src/main/java/project/cmpt276/androidui/walkingschoolbus/editUserInfo.java
+++ app/src/main/java/project/cmpt276/androidui/walkingschoolbus/editUserInfo.java
@@ -3,6 +3,9 @@ package project.cmpt276.androidui.walkingschoolbus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import project.cmpt276.model.walkingschoolbus.GroupCollection;
@@ -20,24 +23,52 @@ public class editUserInfo extends AppCompatActivity {
     private GroupCollection groupList;
     private SharedValues sharedValues;
 
+    private EditText updateAddress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user_info);
+        user = User.getInstance();
+        updateAddress  = findViewById(R.id.updateAddress);
+        updateAddress.setText(user.getAddress(), TextView.BufferType.EDITABLE);
+//
 
 //        sharedValues = SharedValues.getInstance();
 //        proxy = ProxyBuilder.getProxy(getString(R.string.apiKey), sharedValues.getToken());
-        user = User.getInstance();
+
 //        Log.i(TAG, ""+user.toString());
 //        groupList = GroupCollection.getInstance();
 
         setupGreeting();
+        setUpDoneButton();
     }
 
 
     private void setupGreeting() {
         TextView view = findViewById(R.id.testGreeting);
-        view.setText(user.getName());
+        view.setText(user.getName() + " " + user.getTeacherName() + " " + " you live at " + user.getAddress());
+    }
+
+
+    private void setUpDoneButton()
+    {
+        Button button = findViewById(R.id.doneBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newAddress = updateAddress.getText().toString();
+
+                if(!newAddress.equals(""))
+                {
+                    user.setAddress(newAddress);
+
+                }
+
+                finish();
+            }
+        });
     }
 }
