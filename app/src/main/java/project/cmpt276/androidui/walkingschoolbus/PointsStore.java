32988ff362ada4b91cf1696f13444adfd50eb6a0
Jerry <jhf2@sfu.ca>
2018-04-04 21:05:59 -0700

    Gameification Page
     - Added horizontal scrollbar to allow more items to be displayed in the store.
     - Fixed problem with images being squished as more items are added to the store.

--- app/src/main/java/project/cmpt276/androidui/walkingschoolbus/PointsStore.java
+++ app/src/main/java/project/cmpt276/androidui/walkingschoolbus/PointsStore.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
@@ -12,21 +13,36 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PointsStore extends AppCompatActivity {
 
-    private final int row = 5;
-    private final int col = 4;
+    //Conversion: px = dp * (dpi / 160)
+
+    private final int row = 20;
+    private final int col = 3;
     private Button[][] buttons = new Button[row][col];
+
+    private int imgW = 90;
+    private int imgH = 250;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points_store);
         createStoreStock();
+        displayCurrency();
+    }
+
+    private void displayCurrency() {
+        //TODO -- Grab user's points from server.
+        int points = 0;
+        TextView t = findViewById(R.id.userCurrency);
+        t.setText("Balance: " + points);
     }
 
 
@@ -35,17 +51,9 @@ public class PointsStore extends AppCompatActivity {
         TableLayout table = findViewById(R.id.storeStock);
         for(int i = 0; i < col; i++){
             TableRow tableRow = new TableRow(this);
-            tableRow.setLayoutParams(new TableLayout.LayoutParams(
-                    TableLayout.LayoutParams.MATCH_PARENT,
-                    TableLayout.LayoutParams.MATCH_PARENT,
-                    1.0f));
             table.addView(tableRow);
             for(int j = 0; j < row; j++){
                 final Button item = new Button(this);
-                item.setLayoutParams(new TableRow.LayoutParams(
-                        TableLayout.LayoutParams.MATCH_PARENT,
-                        TableLayout.LayoutParams.MATCH_PARENT,
-                        1.0f));
                 final int r = i;
                 final int c = j;
                 item.setOnClickListener(v -> buttonAction(r,c));
@@ -62,10 +70,10 @@ public class PointsStore extends AppCompatActivity {
         for(int i = 0; i < row; i++){
             for(int j = 0 ; j < col; j++){
                 Button button = buttons[i][j];
-                button.setMinimumWidth(84);
-                button.setMaxWidth(84);
-                button.setMinimumHeight(128);
-                button.setMaxHeight(128);
+                button.setMinimumWidth(imgW);
+                button.setMaxWidth(imgW);
+                button.setMinimumHeight(imgH);
+                button.setMaxHeight(imgH);
             }
         }
     }
@@ -76,18 +84,19 @@ public class PointsStore extends AppCompatActivity {
         //Setting image.
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
-                int imgWidth = 21;
-                int imgHeight = 32;
                 Bitmap originalImage = BitmapFactory.decodeResource(getResources(),R.drawable.batman);
-                Bitmap scaledImage = Bitmap.createScaledBitmap(originalImage, imgWidth,imgHeight,true);
+                Bitmap scaledImage = Bitmap.createScaledBitmap(originalImage, imgW,imgH,true);
                 Resources resources = getResources();
-                buttons[i][j].setBackground(new BitmapDrawable(resources,scaledImage));
+                buttons[i][j].setForeground(new BitmapDrawable(resources,scaledImage));
+                buttons[i][j].setBackgroundResource(R.drawable.button_border);
             }
         }
     }
 
-    //action performed when a button is pressed.
+    //Update the currency on click if a transaction is made.
     private void buttonAction(int i, int j){
+        //TODO -- Purchasing an avatar logic.
+        displayCurrency();
         Toast.makeText(PointsStore.this,"Selected " + i +", "+ j, Toast.LENGTH_SHORT).show();
     }
 
